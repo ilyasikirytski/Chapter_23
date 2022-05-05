b9124e32b1583684fde897cf0f3d848275c07956
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-05-06 00:40:17 +0300

    task_4 initial commit

---
+++ src/tasks/task_4/Main.java
@@ -0,0 +1,12 @@
+package tasks.task_4;
+
+/*
+Паттерн Composite. Разработать структуру данных
+для хранения информации о государствах,
+их административных частях и городах.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
